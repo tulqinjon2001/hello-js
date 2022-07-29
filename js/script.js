f978d8e32a24515692b0b957446d4eb3431bbfa2
tulqinjon2001 <bobomirzayevtulqin@gmail.com>
2022-07-29 15:38:50 +0500

    Add next project

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 var beginUserInformation = alert("Welcome User!");
 
-var firstNameUser = prompt("Enter your first name:", "Bolta").trim(),
-    lastNameUser = prompt("Enter your last name?", "Qilichev").trim(),
+var firstNameUser = prompt("Enter your first name:", "To'lqinjon").trim(),
+    lastNameUser = prompt("Enter your last name?", "Bobomirzayev").trim(),
     ageUser = prompt("Enter your age:", "21").trim(),
     occupationUser = prompt("Enter your profession", "Cyber Security").trim(),
     addressUser = prompt("Enter your address", "Qashqadaryo").trim();
@@ -16,3 +16,12 @@ User address: ${addressUser}`);
 confirm("Are you sure your data is correct?");
 
 var endUserInformation = alert("Thank You very much!");
+
+function footballTeam(age) {
+  if(age <= 18){
+    alert("Welcome to my site");
+  } else {
+    alert("You are late. Will you bring your son");
+  }
+}
+footballTeam(ageUser);
